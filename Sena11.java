a3d67b4587a0649a44ce5c9ee62ec2c916d7d8cb
sbslayos <113193225+sbslayos@users.noreply.github.com>
2022-10-29 00:18:49 -0500

    Add files via upload

---
+++ Sena11.java
@@ -0,0 +1,21 @@
+
+package sena11;
+
+
+
+public class Sena11 {
+
+    
+    public static void main(String[] args) {
+        
+        int i = 0;
+         while( i < 1000 ){
+            System.out.println(+i);
+             i++;
+           
+        }
+
+    } 
+}
+
+// Profe las clases salen como el ejercicio 11, pero en la guia es el 12
